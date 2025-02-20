9f21af3d46919f45e98f1ef7f3c1416a3e9b02a6
Loris Sogliuzzo <sogliuzzo.loris@gmail.com>
2025-02-20 14:28:42 +0100

    .

--- Flot/fordFulkerson.m
+++ Flot/fordFulkerson.m
@@ -1,28 +1,33 @@
 function [preds, flows] = fordFulkerson(C, s, t)
-    % Calcul d'une solution de flow maximal
-    dim = size(C);
-    nSommet = dim(1);
+    % Calcul d'une solution de flux maximal
+    nSommet = size(C, 1);
     residuel = C;
-    flows = [];
-    preds = [];
+    flows = zeros(1, 0);
+    preds = zeros(nSommet, 0);
+    
     [sortie, parent] = cheminAugmentant(residuel, s, t);
+    
     while sortie == 1
-        % Recherche du flow minimun
+        % Recherche du flux minimum
         flowMin = inf;
         i = t;
         while i ~= s
             flowMin = min(flowMin, residuel(parent(i), i));
             i = parent(i);
         end
-        % Calcul du nouveau graph résiduel
+        
+        % Mise à jour du graphe résiduel
         i = t;
-        while i~= s
+        while i ~= s
             residuel(parent(i), i) = residuel(parent(i), i) - flowMin;
-            residuel(i, parent(i)) = residuel(i, parent(i)) + flowMin;
+            residuel(i, parent(i)) = residuel(i, parent(i)) + flowMin; % Ajout du chemin inverse
             i = parent(i);
         end
+        
+        % Sauvegarde du flux et du prédécesseur
         flows(end + 1) = flowMin;
-        preds(:, end + 1) = parent;
+        preds(:, end + 1) = parent(:);
+        
         % Recherche d'un nouveau chemin augmentant
         [sortie, parent] = cheminAugmentant(residuel, s, t);
     end
